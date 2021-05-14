717e65c2c6e4d3720534a68dbe83fde777c3e2a1
Dulipe <filipe_opc2@hotmail.com>
2021-05-14 18:23:37 -0300

    finalizado

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const res1 = document.getElementById('res1')
 const btn1 = document.getElementById('btn1')
 
 function round() {
+    if (mathRound.value.length == 0) {
+        alert('Digite um numero decimal ex: 5,50')
+    } else {
     let inteiro = Math.round(mathRound.value)
     res1.innerHTML = `<p>O numero arredondado é <strong>${inteiro}</strong></p>`
+    }
 }
 
 btn1.addEventListener('click', round)
@@ -18,8 +22,12 @@ const n2 = document.getElementById('math-pow2')
 const res2 = document.getElementById('res2')
 
 function pow() {
+    if (n1.value.length == 0 || n2.value.length == 0) {
+        alert('Digite um numero base e expoente !')
+    } else {
     let potencia = Math.pow(n1.value,n2.value)
-    res2.innerHTML = `A potencia de ${n1.value} elevado a ${n2.value} é <strong>${potencia}</strong>`
+    res2.innerHTML = `A potencia de ${n1.value} elevado a ${n2.value} é <strong>${potencia.toFixed(2)}</strong>`
+    }
 }
 
 btn2.addEventListener('click', pow)
@@ -30,10 +38,14 @@ const raio = document.getElementById('raio')
 const res3 = document.getElementById('res3')
 
 function circulo() {
+    if (raio.value.length == 0) {
+        alert('Digite um numero!')
+    } else {
     let circunferencia = 2 * Math.PI * (raio.value)
     let area = Math.PI * Math.pow(raio.value,2)
 
-    res3.innerHTML = `A circunferencia de ${raio.value} é <strong>${circunferencia}</strong> <br> e a area de ${raio.value} é <strong>${area}</strong>`
+    res3.innerHTML = `A circunferencia de ${raio.value} é <strong>${circunferencia.toFixed(2)}</strong> <br> e a area de ${raio.value} é <strong>${area.toFixed(2)}</strong>`
+    }
 }
 
 btn3.addEventListener('click', circulo)
@@ -44,11 +56,15 @@ const cubo = document.getElementById('cubo')
 const res4 = document.getElementById('res4')
 
 function area() {
+    if (cubo.value.length == 0) {
+        alert('Digite um numero!')
+    } else {
     let areaBase = Math.pow(cubo.value,2)
     let areaTotal = areaBase * 6
     let volume = Math.pow(cubo.value,3)
 
-    res4.innerHTML = `A area base de ${cubo.value} é <strong>${areaBase}</strong> <br> A area total  de ${cubo.value} é <strong>${areaTotal}</strong> <br> O volume do cubo de ${cubo.value} é <strong>${volume}</strong>`
+    res4.innerHTML = `A area base de ${cubo.value} é <strong>${areaBase.toFixed(2)}</strong> <br> A area total  de ${cubo.value} é <strong>${areaTotal.toFixed(2)}</strong> <br> O volume do cubo de ${cubo.value} é <strong>${volume.toFixed(2)}</strong>`
+    }
 }
 
 btn4.addEventListener('click', area)
@@ -59,9 +75,13 @@ const raiz = document.getElementById('raiz')
 const res5 = document.getElementById('res5')
 
 function raizQuadrada() {
+    if (raiz.value.length == 0) {
+        alert('Digite um numero!')
+    } else {
     let num = Math.sqrt(raiz.value)
 
-    res5.innerHTML = `A raiz quadrada de ${raiz.value} é <strong>${num}</strong>`
+    res5.innerHTML = `A raiz quadrada de ${raiz.value} é <strong>${num.toFixed(2)}</strong>`
+    }
 }
 
 btn5.addEventListener('click', raizQuadrada)
